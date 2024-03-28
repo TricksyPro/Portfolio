c4742fad58a2daa211ccc0f04ff5e402d30db9b5
TricksyPro <165245564+TricksyPro@users.noreply.github.com>
2024-03-28 13:24:38 +0800

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,266 @@
+<template>
+<nav class="navbar navbar-expand-lg bg-info sticky-top py-3">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white title ms-2 me-4" href="#home">Tricksy</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link text-white" href="#home">Home</a>
+        <a class="nav-link text-white" href="#skills">Skills</a>
+        <a class="nav-link text-white" href="#projects">Projects</a>
+        <a class="nav-link text-white" href="#contact">Contact</a>
+        <a class="nav-link text-white" href="#about">About</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<div class="text-center py-4 mb-5">
+  <h1 class="title text-info" style="font-size: 140px;">
+    Tricksy Kaye 
+  </h1>
+</div>
+
+<section id="home"></section>
+
+<div class="container-fluid px-4 py-4">
+    <div class="row">
+      <div class="col-lg-1 col-md-0">
+
+      </div>
+      <div class="col-lg-4 mb-3">
+        <img src="portrait.png" class="border-0 shadow img-fluid img-thumbnail" alt="...">
+      </div>
+      <div class="col-lg-6">
+        <div class="card border-0 shadow">
+          <div class="title card-header border-0 text-start p-3">
+              <h2>Get to know me!</h2>
+          </div>
+          <div class="card-body">
+            <form class="text-start">
+              <div class="form-group">
+                <label class="title fs-4">Name</label>
+                <input type="text" class="form-control content bg-transparent border-0 fs-5 mb-3" value="Tricksy Kaye Saldivar" disabled>
+              </div>
+              <div class="form-group">
+                <label class="title fs-4">Educational Information</label>
+                <input type="text" class="form-control content bg-transparent border-0 fs-5 mb-3" value="Bachelor of Science in Information Technology - 3rd Year College" disabled>
+              </div>
+              <div class="form-group">
+                <label class="title fs-4">Birth Information</label>
+                <input type="text" class="form-control content bg-transparent border-0 fs-5 mb-3" value="April 11, 2003 at Bonifacio, Misamis Occidental City" disabled>
+              </div>
+              <div class="form-group">
+                <label class="title fs-4">Hobbies</label>
+                <input type="text" class="form-control content bg-transparent border-0 fs-5 mb-3" value="Listening to songs, Watching sunset" disabled>
+              </div>
+              <div class="form-group">
+                <label class="title fs-4">Favorite Foods</label>
+                <input type="text" class="form-control content bg-transparent border-0 fs-5 mb-3" value="Soups, Exotic foods" disabled>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+
+<section id="skills"></section>
+
+<div class="container-fluid px-5">
+  <h1 class="title text-center mb-5">Skills</h1>
+    <div class="row">
+      <div class="col-lg-6">
+          <div class="card mb-3 border-0 shadow">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="strategy.png" class="img-fluid rounded-start p-4" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title title py-4">Strategic/Critical thinker</h5>
+                <p class="content card-text">
+                    I excel in strategic thinking, adept at analyzing complex situations and devising actionable plans to achieve long-term objectives. My ability to anticipate challenges and pivot accordingly ensures effective navigation through dynamic environments.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+          <div class="card mb-3 border-0 shadow">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="communication.png" class="img-fluid rounded-start p-4" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title title py-4">Communicator</h5>
+                <p class="content card-text">
+                  I possess exceptional communication skills, adept at conveying ideas clearly and persuasively across diverse audiences. Whether in written or verbal form, I excel at fostering understanding and driving engagement with precision and empathy.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+          <div class="card my-3 border-0 shadow">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="t-management.png" class="img-fluid rounded-start p-4" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title title py-4">Time Management</h5>
+                  <p class="content card-text">I excel in time management, efficiently prioritizing tasks and maximizing productivity to meet deadlines consistently. 
+                    With a keen eye for detail and a proactive approach, I optimize schedules and resources to achieve goals effectively.
+                  </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+          <div class="card my-3 border-0 shadow">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="technical.png" class="img-fluid rounded-start p-4" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title title py-4">Technicality</h5>
+                <p class="content card-text">
+                  I am proficient in technical skills, demonstrating mastery in various tools, software, and methodologies relevant to my field. My ability to troubleshoot and innovate allows me to tackle complex problems with confidence and efficiency.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+
+<section id="projects"></section>
+
+<div class="container-fluid px-5 py-4">
+  <h1 class="title text-center mb-5">Projects</h1>
+    <div class="row">
+      <div class="col-lg-6 py-4">
+        <a class="text-decoration-none" href="">
+          <div class="card border-0 shadow zoom-out-effect">
+            <img src="https://scontent-mnl1-1.xx.fbcdn.net/v/t39.30808-6/434344165_375245002148835_4781423437953767180_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeH32e0cmlx4XTUve5x7sRPns6QsJWzk37OzpCwlbOTfs8P4jatN6fnSb6wOkM6xlCv9U5GR2Q5BWEb_8qfq88kF&_nc_ohc=6XQHtTb24jcAX9nWDVV&_nc_ht=scontent-mnl1-1.xx&oh=00_AfCQJ_lRq6LhCkPuBSIsoK--TTGxXDZYut-b-8o4tvoXcA&oe=660A18F5" class="card-img" alt="...">
+            <div class="card-img-overlay">
+              <h5 class="card-title title fs-2 text-dark">Currency Converter</h5>
+              <p class="card-text content fs-5 text-dark">This project revolves around currency conversion from one currency to another, utilizing APIs as a crucial component. Through the integration of APIs, I facilitated seamless and accurate currency exchange functionality within the project.</p>
+            </div>
+          </div>
+        </a>    
+      </div>
+      <div class="col-lg-6 py-4">
+        <a class="text-decoration-none" href="">
+          <div class="card border-0 shadow zoom-out-effect">
+            <img src="https://scontent.fceb2-1.fna.fbcdn.net/v/t39.30808-6/434385062_375245218815480_6306445472955477310_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGX9UWfQuKQRYHZHuONm0GXM6lLpwg86pUzqUunCDzqlVLv2WNMQmPe7Poc44WVQ6k5wIBTWHlACe_R41ipaurb&_nc_ohc=X0UrLrVVnLsAX9hzFW8&_nc_ht=scontent.fceb2-1.fna&oh=00_AfCb5qliHDQr4TmlLYj4fDJDvedn-bKHVMCcSnmSj1Ls5g&oe=6609066E" class="card-img" alt="...">
+            <div class="card-img-overlay special">
+              <h5 class="card-title title fs-2 text-dark">Motivation Quotes Generator</h5>
+              <p class="card-text content fs-5 text-dark">This project generates random motivational quotes using an API, inspiring individuals with uplifting messages.</p>
+            </div>
+          </div>
+        </a>    
+      </div>
+    </div>
+</div>
+
+<section id="contact"></section>
+        
+<h1 class="pt-4 mb-4 text-center fw-bold title">Contact</h1>
+
+<div class="container-fluid">
+    <div class="row justify-content-evenly px-lg-0 px-md-3 px-5">
+        <div class="col-lg-5 col-md-4 text-center py-4 my-3 mx-3">
+          <a href="mailto:tricksykayesaldivar340@gmail.com" class="text-dark text-decoration-none">
+            <h4 class="zoom-out-effect fw-bold content"><i class="fas fa-envelope"></i> Email: tricksykayesaldivar340@gmail.com</h4>
+          </a>  
+        </div>
+        <div class="col-lg-5 col-md-4 text-center py-4 my-3 mx-3">
+          <h4 class="zoom-out-effect fw-bold content"><i class="fas fa-phone"></i> Contact Number: +639339272948</h4>
+        </div>
+        <div class="col-lg-5 col-md-4 text-center py-4 my-3 mx-3">
+          <a href="https://www.facebook.com/profile.php?id=100093189266626" class="text-dark text-decoration-none">
+            <h4 class="zoom-out-effect fw-bold content"><i class="fab fa-facebook"></i> Facebook: Kaye</h4>
+          </a>  
+        </div>
+    </div>
+</div>
+
+<section id="about"></section>
+        
+<h1 class="pt-4 mb-4 text-center fw-bold title">About</h1>
+
+<div class="container-fluid">
+    <div class="row justify-content-center px-lg-0 px-md-3 px-5">
+        <div class="col-lg-12 text-center py-4 my-3 mx-3">
+            <h4 class="content">Welcome to my portfolio, where creativity meets functionality. Explore a collection of my projects showcasing innovative design, meticulous craftsmanship, and seamless user experiences. From web development to graphic design, each project reflects my passion for transforming ideas into impactful solutions.</h4>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+.content {
+  font-family: "PT Sans", sans-serif;
+  font-weight: 400;
+  font-style: italic;
+}
+
+.card-img-overlay {
+  margin-top: 200px;
+}
+
+@media (max-width: 768px) {
+  .card-img-overlay {
+    margin-top: 0px; 
+  }
+
+  .special {
+    margin-top: 100px;
+  }
+}
+
+.title {
+  font-family: "Pacifico", cursive;
+  font-weight: 400;
+  font-style: normal;
+}
+.zoom-out-effect {
+  transition: transform 0.3s ease;
+}
+
+.zoom-out-effect:hover {
+  transform: scale(0.97);
+}
+</style>
